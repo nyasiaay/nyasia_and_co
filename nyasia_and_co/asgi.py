d1a2b16bb085700f777b3310f38215a259d7cde1
nyasiaay <nyasia.aludra@ui.ac.id>
2023-09-11 14:01:47 +0700

    hai

---
+++ nyasia_and_co/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for nyasia_and_co project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nyasia_and_co.settings')
+
+application = get_asgi_application()
