720a1998d66bb16d08c86cdbe9fb8f900879c6ab
nyasiaay <nyasia.aludra@ui.ac.id>
2023-11-24 14:25:56 +0700

    add registration flutter

--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate, login as auth_login
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.models import User
 
 @csrf_exempt
 def login(request):
@@ -47,3 +48,21 @@ def logout(request):
         "message": "Logout gagal."
         }, status=401)
 
+@csrf_exempt
+def register(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        new_user = User.objects.create_user(username=username, password=password)
+            
+        return JsonResponse({
+            "status": True,
+            "message": "Account created successfully!",
+            "user_id": new_user.id,
+        }, status=200)
+    
+    return JsonResponse({
+        "status": False,
+        "message": "Invalid request method."
+    }, status=405)
